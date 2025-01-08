92126b66e2193a3fa883db5538f813b534dba021
tatyana evdashkina <evdashkina@gmail.com>
2025-01-08 20:03:57 +0700

    Add project files.

---
+++ addressbook_tests-white/tests/GroupCreationTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace addressbook_tests_white
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void TestGroupCreation()
+        {
+
+            //List<GroupData> 
+                int oldGroups = app.Groups.GetGroupList();
+
+
+            GroupData newGroup = new GroupData()
+            {
+                Name = "white"
+            };
+
+            app.Groups.Add(newGroup);
+
+
+            //List<GroupData> 
+            int    newGroups = app.Groups.GetGroupList();
+            oldGroups = oldGroups + 1;
+            //oldGroups.Add(newGroup);
+           // oldGroups.Sort();
+           // newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+
+        }
+    }
+}
